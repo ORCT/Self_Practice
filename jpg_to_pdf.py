266a6e704ed075c578cc3f56b5a6aee39bebec88
ORCT <sonic407@naver.com>
2021-09-18 01:48:42 +0900

    fix the problem that order of file

--- jpg_to_pdf.py
+++ jpg_to_pdf.py
@@ -1,28 +1,18 @@
-#problem :
-#1. the order of images were ignored in the convert progress although thar images have each of their numbers
 import os
-from img2pdf import convert
+import img2pdf
+import natsort
+
+directory = './'
 
 with open('out.pdf', 'wb') as f:
-    pdf_list=[]
-    pdf_list1=[]
+    pdf_list = []
+    files = natsort.natsorted(os.listdir(directory))
+    for file in files:
+        if file.endswith('.jpg') or file.endswith('.png'):
+            pdf_list.append(file)
 
-    for root, dirs, files in os.walk('./'):
-        for idx, file in enumerate(files):
-            fname, ext = os.path.splitext(file)
-            if file.endswith('.png'):
-                pdf_list.append(file)
-                pdf_list1 = sorted(pdf_list)
-                for i in range(len(pdf_list)):
-                    if i == int(fname[3:]):
-                #        pdf_list1.append(pdf_list[i])
-            #print(fname[3:])
-                
-
-#    pdf = convert(pdf_list)
-#    f.write(pdf)
-#new = sorted(pdf_list)
-#new1 = list(map(int,new))
-#print(pdf_list)
-print(pdf_list1)
-#print(new1)
+    pdf = img2pdf.convert(pdf_list)
+    f.write(pdf)
+    
+#if you see the TypeError: blah blah 'str', you must check the directory first!
+#you can get the information about 'encode' or 'byte' etc, but it will be probably not the answer.
